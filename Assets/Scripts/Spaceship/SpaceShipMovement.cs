7de8293fb55d266dec21f521934f00a05a0a6a77
Stephen Ho <stephen.ho.232@gmail.com>
2021-04-19 23:40:01 +0700

    - Add touch controller

---
+++ Assets/Scripts/Spaceship/SpaceShipMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+
+public class SpaceShipMovement : MonoBehaviour
+{
+    public Canvas canvas;
+    public float sensitivity;
+    public float screenToGameMultiplier;
+    bool isTouching;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenToGameMultiplier = GameHelper.SizeOfCamera().y / Screen.height;
+        isTouching = false;
+        OnEnableTouch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTouching) return;
+        var screen_delta = LeanGesture.GetScreenDelta();
+        if (screen_delta == Vector2.zero) return;
+        TranslateShip(screen_delta);
+    }
+    private void OnDestroy()
+    {
+        OnDisableTouch();
+    }
+    private void OnEnableTouch()
+    {
+        LeanTouch.OnFingerDown += OnTouchDown;
+        LeanTouch.OnFingerUp += OnTouchUp;
+    }
+    private void OnDisableTouch()
+    {
+        LeanTouch.OnFingerDown -= OnTouchDown;
+        LeanTouch.OnFingerUp -= OnTouchUp;
+    }
+    public void OnTouchDown(LeanFinger fingers)
+    {
+        isTouching = true;
+    }
+    public void OnTouchUp(LeanFinger fingers)
+    {
+        isTouching = false;
+    }
+  
+    void TranslateShip(Vector2 delta)
+    {
+        var camera = Camera.main;
+        if(camera!=null)
+        {
+            var screen_pts = camera.WorldToScreenPoint(transform.position);
+            screen_pts += (Vector3)delta * sensitivity /** screenToGameMultiplier*/;
+
+            var world_pts = camera.ScreenToWorldPoint(screen_pts);
+
+            if (world_pts.x <= -2.5f) world_pts.x = -2.5f;
+            if (world_pts.x >= 2.5f) world_pts.x = 2.5f;
+            world_pts.y = -4;
+            transform.position = world_pts;
+        }
+        else
+        {
+            Debug.LogError("Camera is null");
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/GameHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameHelper
+{
+    public static Vector2 sizeOfCamera = Vector2.zero;
+    public static Vector2 SizeOfCamera()
+    {
+        if(sizeOfCamera == Vector2.zero)
+        {
+            ReCalculateSizeOfCamera();
+        }
+        return sizeOfCamera;
+    }
+    public static void ReCalculateSizeOfCamera()
+    {
+        Vector2 A = new Vector2();
+        A.y = Camera.main.orthographicSize * 2;
+        A.x = (Camera.main.aspect * Camera.main.orthographicSize) * 2;
+        sizeOfCamera = A;
+    }
+    
+}
