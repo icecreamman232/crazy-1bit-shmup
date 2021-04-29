69cb0d9dcf18dee1d94a39ef4f7b396edf7c87ab
Stephen Ho <stephen.ho.232@gmail.com>
2021-04-29 23:10:26 +0700

    - Add scene menu

---
+++ Assets/Scripts/UI/UIMenuCanvasController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuCanvasController : MonoBehaviour
+{
+    Animator menu_animator;
+    string next_scene;
+    bool isDone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AnimationFinished()
+    {
+        SceneLoader.Instance.LoadScene(next_scene);
+    }
+    public void CallbackToTransition(string scene_name)
+    {
+        next_scene = scene_name;
+        menu_animator.Play("menu_outtro");
+    }
+    
+}
---
+++ Assets/Scripts/Utility/SceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    #region Singleton
+    public static SceneLoader Instance;
+    #endregion
+
+    public Animator scene_loader_animator;
+    public Image loading_bg;
+    public Image loading_logo;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(loading_bg.gameObject.transform.parent);
+    }
+    void Start()
+    {
+        RectTransform rt = loading_bg.rectTransform;
+        rt.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadScene(string scene_name)
+    {
+        StartCoroutine(LoadAsyncScene(scene_name));
+    }
+    IEnumerator LoadAsyncScene(string scene_name)
+    {
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(scene_name);
+        loading_bg.gameObject.SetActive(true);
+        loading_logo.gameObject.SetActive(true);
+        scene_loader_animator.Play("loading_logo_anim");
+        while (!asyncload.isDone)
+        {
+            scene_loader_animator.Play("New State");
+            loading_bg.gameObject.SetActive(false);
+            loading_logo.gameObject.SetActive(false);
+            yield return null;
+        }
+    }
+}
